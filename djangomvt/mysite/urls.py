7a4efe01cbef1052bfda1320a1958df0e6bd4edf
Petro <petrotymchuck@gmail.com>
2025-09-23 20:05:04 +0300

    Add image URL settings and update urlpatterns to serve images

--- djangomvt/mysite/urls.py
+++ djangomvt/mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -24,3 +26,5 @@ urlpatterns = [
     path('', views.homePage, name='homePage'),
     path('about/', views.about, name='about'),
 ]
+
+urlpatterns += static(settings.IMAGES_URL, document_root=settings.IMAGES_ROOT)
