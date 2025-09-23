9c08acdfd855e91ed7c12cfd1ffc88ac5a739f60
Petro <petrotymchuck@gmail.com>
2025-09-23 18:38:19 +0300

    first commit

---
+++ djangomvt/categories/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='category_images/', blank=True, null=True)
+
