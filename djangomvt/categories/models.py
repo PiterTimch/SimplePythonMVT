09a3f70e80f63bf9797a5fc0f99d1c1f8d67b600
Petro <petrotymchuck@gmail.com>
2025-09-23 19:46:28 +0300

    Refactor code structure for improved readability and maintainability

--- djangomvt/categories/models.py
+++ djangomvt/categories/models.py
@@ -3,5 +3,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to='category_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
