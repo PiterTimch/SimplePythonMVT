cc43ad5a27ba6a10b016216ceb3070d4cc3fa682
Petro <petrotymchuck@gmail.com>
2025-09-24 16:09:51 +0300

    Add create category

--- djangomvt/categories/forms.py
+++ djangomvt/categories/forms.py
@@ -4,8 +4,9 @@ from .models import Category
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        fields = ['name', 'description']
+        fields = ['name', 'slug', 'image']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control',}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control',}),
+            'slug': forms.TextInput(attrs={'class': 'form-control',})
         }
--- djangomvt/categories/urls.py
+++ djangomvt/categories/urls.py
@@ -6,4 +6,5 @@ app_name = 'categories'
 urlpatterns = [
     path('', views.show_categories, name='show_categories'),
     path('add/', views.add_category, name='add_category'),
+    path('delete/<int:category_id>/', views.delete_category, name='delete_category'),
 ]
