281fe49cf6ff68d985d79e088f16ee3b7e75d90b
Petro <petrotymchuck@gmail.com>
2025-09-25 07:35:56 +0300

    Add image name generator

--- djangomvt/categories/models.py
+++ djangomvt/categories/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+import uuid
+
+def image_upload_to(instance, filename):
+    ext = "webp"
+    filename = f"{uuid.uuid4().hex}.{ext}"
+    return f'images/{filename}'
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    image = models.ImageField(upload_to=image_upload_to, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
