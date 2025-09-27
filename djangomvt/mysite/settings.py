49740d942ebbe21b64a87565e3d67179112b99d9
Petro <petrotymchuck@gmail.com>
2025-09-27 19:06:48 +0300

    add product model and migration

--- djangomvt/mysite/settings.py
+++ djangomvt/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'categories.apps.CategoriesConfig',
+    'products.apps.ProductsConfig',
 ]
 
 MIDDLEWARE = [
